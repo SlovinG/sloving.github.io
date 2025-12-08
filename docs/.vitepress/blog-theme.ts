95154edfac51f12a8461b6ecfb382cc35513e887
chenhhung <chenhhuang@tencent.com>
2025-12-08 11:48:45 +0800

    Deploy updates from local project folder

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -69,17 +69,21 @@ const blogTheme = getThemeConfig({
     // 友链
     friend: [
         {
+            nickname: '书灏',
+            des: '最爱湖东行不足，绿杨阴里白沙提',
+            avatar: 'https://zhaowuya.s3.bitiful.net/avatar.png',
+            url: 'https://zhaowuya.top/',
+        },
+        {
             nickname: '粥里有勺糖',
             des: '你的指尖用于改变世界的力量',
-            avatar:
-                'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
+            avatar: 'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
             url: 'https://sugarat.top',
         },
         {
             nickname: 'Vitepress',
             des: 'Vite & Vue Powered Static Site Generator',
-            avatar:
-                'https://vitepress.dev/vitepress-logo-large.webp',
+            avatar: 'https://vitepress.dev/vitepress-logo-large.webp',
             url: 'https://vitepress.dev/',
         },
     ],
